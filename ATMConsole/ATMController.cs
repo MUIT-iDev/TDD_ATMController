99edbbcda22807a9985758c58f7100a0cf555ff9
kittisak.tos <ruzerie@gmail.com>
2020-01-22 21:40:24 +0700

    TDD Mantra -- 02 green state

--- ATMConsole/ATMController.cs
+++ ATMConsole/ATMController.cs
@@ -6,8 +6,17 @@ namespace ATMConsole
 {
     public class ATMController
     {
-        public bool WithDraw(string username, double amount) {
-            return true;
+        public double Money { get; set; } = 500;
+
+        public bool WithDraw(string username, double amount)
+        {
+            if (Money >= amount)
+            {
+                Money -= amount;
+                return true;
+            }
+
+            return false;
         }
 
     }
