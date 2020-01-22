7fea6c27678567e12cf7b531c6ab246af4bc87f9
kittisak.tos <ruzerie@gmail.com>
2020-01-22 21:56:03 +0700

    refactor test code

--- ATMConsoleTest/UnitTest1.cs
+++ ATMConsoleTest/UnitTest1.cs
@@ -8,19 +8,24 @@ namespace ATMConsoleTest
         [Fact(DisplayName = "ผู้ใช้กดเงินออกจากตู้ ข้อมูลถูกต้อง ระบบทำการหักเงินแล้วนำเงินออกมา")]
         public void Test1()
         {
-            var atm = new ATMConsole.ATMController();
-            var result = atm.WithDraw("john", 500);
-            Assert.True(result);
-            Assert.Equal(0, atm.Money);
+            //sut = System Under Test
+            var sut = new ATMConsole.ATMController();
+            var actual = sut.WithDraw("john", 500);
+            Assert.True(actual);
+
+            var expectedMoney = 0;
+            Assert.Equal(expectedMoney, sut.Money);
         }
 
         [Fact(DisplayName = "ผู้ใช้กดเงินออกจากตู้ แต่เงินในบัญชีไม่พอ ระบบทำการแจ้งเตือน")]
         public void Test2()
         {
-            var atm = new ATMConsole.ATMController();
-            var result = atm.WithDraw("john", 1000);
-            Assert.False(result);
-            Assert.Equal(500, atm.Money);
+            var sut = new ATMConsole.ATMController();
+            var actual = sut.WithDraw("john", 1000);
+            Assert.False(actual);
+
+            var expectedMoney = 500;
+            Assert.Equal(expectedMoney, sut.Money);
         }
     }
 }
