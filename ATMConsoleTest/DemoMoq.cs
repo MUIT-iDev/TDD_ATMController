1c60ae6bd2e734d380e5782e966a369b50c54f6a
kittisak.tos <ruzerie@gmail.com>
2020-01-27 21:04:14 +0700

    start moq with interface log

---
+++ ATMConsoleTest/DemoMoq.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ATMConsoleTest
+{
+    public class DemoMoq
+    {
+        [Fact]
+        public void DemoMoq01()
+        {
+
+        }
+    }
+}
