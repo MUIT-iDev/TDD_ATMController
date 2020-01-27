545e12683368cd19a61b666cc04400eaa8c9164b
kittisak.tos <ruzerie@gmail.com>
2020-01-27 21:11:12 +0700

    moq create class from interface error test case

--- ATMConsoleTest/DemoMoq.cs
+++ ATMConsoleTest/DemoMoq.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Moq;
 
 namespace ATMConsoleTest
 {
@@ -10,6 +11,19 @@ namespace ATMConsoleTest
         [Fact]
         public void DemoMoq01()
         {
+            //create mock object
+            var mock = new Moq.MockRepository(Moq.MockBehavior.Default);
+            var logMocking = mock.Create<ATMConsole.ILogFile>();
+
+            //get object from interface
+            var log = logMocking.Object;
+            log.WriteWithdraw("sakul", 123);
+
+            //test mocking
+            logMocking.Verify(o => 
+                o.WriteWithdraw(
+                    It.Is<string>(acul => acul == "sakul"), 
+                    It.Is<double>(acul => acul == 999)));
 
         }
     }
