c1594640d7b3a9fb86d33321654078c7624501e0
kittisak.tos <ruzerie@gmail.com>
2020-01-27 19:41:50 +0700

    add wrong case

--- ATMConsoleTest/UnitTest1.cs
+++ ATMConsoleTest/UnitTest1.cs
@@ -7,12 +7,26 @@ namespace ATMConsoleTest
     {
         [Theory(DisplayName = "ผู้ใช้กดเงินออกจากตู้ ข้อมูลถูกต้อง ระบบทำการหักเงินแล้วนำเงินออกมา")]
         [InlineData("john", 500, 0)]
+        [InlineData("john", 450, 50)]
+        [InlineData("john", 1, 499)]
         public void Test1(string username, double withdrawAmount, double expectedMoney)
         {
             //sut = System Under Test
             var sut = new ATMConsole.ATMController();
             var actual = sut.WithDraw(username, withdrawAmount);
             Assert.True(actual);
+            Assert.Equal(expectedMoney, sut.Money);
+        }
+
+        [Theory(DisplayName = "ผู้ใช้กดเงินออกจากตู้ ข้อมูลไม่ถูกต้อง ระบบทำการแจ้งเตือน")]
+        [InlineData("john", 0, 500)]
+        [InlineData("john", -1, 500)]
+        public void Test3(string username, double withdrawAmount, double expectedMoney)
+        {
+            //sut = System Under Test
+            var sut = new ATMConsole.ATMController();
+            var actual = sut.WithDraw(username, withdrawAmount);
+            Assert.False(actual);
             Assert.Equal(expectedMoney, sut.Money);
         }
 
