86bed304f63f36196158d7c480ca808babc6204f
kittisak.tos <ruzerie@gmail.com>
2020-01-27 19:52:25 +0700

    edit working class

--- ATMConsole/ATMController.cs
+++ ATMConsole/ATMController.cs
@@ -6,16 +6,28 @@ namespace ATMConsole
 {
     public class ATMController
     {
-        public double Money { get; private set; } = 500;
+        public IList<Account> Accounts { get; set; }
 
         public bool WithDraw(string username, double amount)
         {
+            var selectedAccount = GetAccountByUsername(username);
+
             const int MinWithdrawAmount = 1;
-            var isWithdrawRequestValid = Money >= amount && amount >= MinWithdrawAmount;
+            var isWithdrawRequestValid = selectedAccount != null && selectedAccount.Balance >= amount && amount >= MinWithdrawAmount;
             if (!isWithdrawRequestValid) return false;
 
-            Money -= amount;
+            selectedAccount.Balance -= amount;
             return isWithdrawRequestValid;
+        }
+
+        public Account GetAccountByUsername(string username)
+        {
+            foreach (var item in Accounts)
+            {
+                if (item.Username == username) return item;
+            }
+
+            return null;
         }
 
     }
